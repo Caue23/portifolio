c77249d5835634a15b10801b4e20fa145dfda102
CaueSantos <cauevinicius231@gmail.com>
2023-09-29 21:21:38 -0300

    atualização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,41 @@
-import styles from './Home.module.scss'
-import Link from 'next/link'
-
+import { useState } from 'react';
+import styles from './Home.module.scss';
+import Link from 'next/link';
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <header className={styles.ContainerImage}>
+    <header className={`${styles.ContainerImage} ${darkMode ? styles.darkTheme : styles.lightTheme}`}>
       <section className={styles.ContentImage}>
-        <img src="/image/caue1.jpg" />
+        <img src="/image/caue1.jpg" alt="Imagem" />
       </section>
       <section className={styles.textImage}>
-          <h3>Olá!</h3>
-          <p>Um pouco sobre mim</p>
-          <a>Me chamo Cauê Vinicius, tenho 28 anos, um apaixonado por tecnologia. Sou estudante de sistemas da informação e desenvolvedor web front end. 
-            Há 1 ano migrei de carreira e acabei me encontrando na área de tecnologia, como desenvolvedor júnior realizava projetos de
-            paginas web e landingPages responsivas e acessíveis. Utilizo as linguagens React-JS, JavaScript, TypeScript, HTML & CSS e
-            a cada dia buscando se aprofundar cada vez mais em novas linguagens.
-          </a>
-            <ul >
-              <li className={styles.SpanImage}>
-                <Link href='/curriculo'>
-                  <button>Currículo</button>
-                </Link>
-                <Link href='/projetos'>
-                  <button>Projetos</button>
-                </Link>
-              </li>
-            </ul>
+        <h3>Olá!</h3>
+        <p>Um pouco sobre mim</p>
+        <a>
+          Me chamo Cauê Vinicius, tenho 28 anos, um apaixonado por tecnologia. Sou estudante de sistemas da informação e
+          desenvolvedor web front end. Há 1 ano migrei de carreira e acabei me encontrando na área de tecnologia, como
+          desenvolvedor júnior realizava projetos de paginas web e landingPages responsivas e acessíveis. Utilizo as
+          linguagens React-JS, JavaScript, TypeScript, HTML & CSS e a cada dia buscando se aprofundar cada vez mais em novas
+          linguagens.
+        </a>
+        <ul>
+          <li className={styles.SpanImage}>
+            <Link href="/curriculo">
+              <button>Currículo</button>
+            </Link>
+            <Link href="/projetos">
+              <button>Projetos</button>
+            </Link>
+          </li>
+        </ul>
+        <button onClick={toggleTheme}>Alternar Tema</button>
       </section>
-      
     </header>
-  )
+  );
 }
